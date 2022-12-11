ebbc434f1ea63aef243c9310928002636f7a024b
loganbeckham <loganbeckham1@gmail.com>
2022-12-11 15:03:56 -0600

    Basic Bootstrap Styling on Landing Page

--- src/App.js
+++ src/App.js
@@ -25,19 +25,31 @@ const App = () => {
 
     return (
         <>
-            <nav className="navbar navrbar-expand-la navbar-light" style={{backgroundColor: '#e3f2fd'}}>
+            <nav className="navbar navrbar-expand-la navbar-light" style={{backgroundColor: 'white'}}>
+                <div className="container-fluid">
+                    <a className="navbar-brand ms-3" href="/">SoundCache</a>
+                    <div className="d-flex">
+                        <a className="me-4" href="/" style={{textDecoration: 'none', color: 'black'}}>Search</a>
+                        <a className="me-4" href="/sessions/new" style={{textDecoration: 'none', color: "black"}}>Log In</a>
+                    </div>
+                </div>
             </nav>
-            <form>
-                <input type="text" onChange={handleUserInput}/>
-            </form>
-            <button onClick={getSamples}>search</button>
-            <div className='row'>
-                {samples.map((sample) => {
-                    return (
-                        <SearchResultCards sample={sample}/>
-                    )
-                })
-                }
+
+            <div className='container'>
+                <div className='d-flex flex-row pt-3 justify-content-center'>
+                    <form className='d-inline-flex' style={{maxWidth: '100vw'}}>
+                        <input className='form-control me-2 my-3' style={{minWidth: '40vw'}} type="text" placeholder='Search Free Samples' onChange={handleUserInput}/>
+                    </form>
+                    <button className='btn btn-outline-primary my-3' style={{minWidth: '125px'}} onClick={getSamples}>search</button>
+                </div>
+                <div className='row'>
+                    {samples.map((sample) => {
+                        return (
+                            <SearchResultCards sample={sample}/>
+                        )
+                    })
+                    }
+                </div>
             </div>
         </>
     )
